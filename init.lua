ac84750da09fcf78365a3ca603cab19ebaa88e07
Ismael Tovar <73203513+ismaeltovar@users.noreply.github.com>
2023-12-16 18:56:13 -0600

    Fixed error

--- init.lua
+++ init.lua
@@ -37,6 +37,14 @@ require("lazy").setup({
 vim.cmd("colorscheme kanagawa-dragon")
 --vim.cmd("colorscheme kanagawa-lotus")
 
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>flg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
 --[[
 :lua require('plugins')
 :set number 
